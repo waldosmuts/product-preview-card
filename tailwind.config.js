9e6bfd36c34f346bdde9b819e4bb50cf94ac8e36
Waldo Smuts <wb.smuts@gmail.com>
2022-07-03 12:54:10 +0200

    added color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     extend: {
       colors: {
         "primary-cyan": "hsl(158, 36%, 37%)",
+        "primary-dark-cyan": "rgb(26, 64, 49)",
         "primary-cream": "hsl(30, 38%, 92%)",
         "neutral-dark-blue": "hsl(212, 21%, 14%)",
         "neutral-gray-blue": "hsl(228, 12%, 48%)",
